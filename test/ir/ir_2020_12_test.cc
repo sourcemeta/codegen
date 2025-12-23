4bf88754230feb8f7edf0a0e49341ec2554dac5a
Juan Cruz Viotti <jv@jviotti.com>
2025-12-23 14:41:41 -0400

    Add `IRImpossible` tests
    Signed-off-by: Juan Cruz Viotti <jv@jviotti.com>

--- test/ir/ir_2020_12_test.cc
+++ test/ir/ir_2020_12_test.cc
@@ -410,3 +410,93 @@ TEST(IR_2020_12, object_with_additional_properties) {
       std::get<IRObject>(result.at(2)).additional->instance_location,
       "/~?additionalProperties~/~P~");
 }
+
+TEST(IR_2020_12, object_with_impossible_property) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "object",
+    "properties": {
+      "foo": false
+    }
+  })JSON")};
+
+  const auto result{
+      sourcemeta::codegen::compile(schema, sourcemeta::core::schema_walker,
+                                   sourcemeta::core::schema_resolver)};
+
+  using namespace sourcemeta::codegen;
+
+  EXPECT_EQ(result.size(), 2);
+
+  EXPECT_TRUE(std::holds_alternative<IRImpossible>(result.at(0)));
+  EXPECT_AS_STRING(std::get<IRImpossible>(result.at(0)).pointer,
+                   "/properties/foo");
+  EXPECT_AS_STRING(std::get<IRImpossible>(result.at(0)).instance_location,
+                   "/foo");
+
+  EXPECT_TRUE(std::holds_alternative<IRObject>(result.at(1)));
+  EXPECT_AS_STRING(std::get<IRObject>(result.at(1)).pointer, "");
+  EXPECT_AS_STRING(std::get<IRObject>(result.at(1)).instance_location, "");
+  EXPECT_EQ(std::get<IRObject>(result.at(1)).members.size(), 1);
+  EXPECT_TRUE(std::get<IRObject>(result.at(1)).members.contains("foo"));
+  EXPECT_FALSE(std::get<IRObject>(result.at(1)).members.at("foo").required);
+  EXPECT_FALSE(std::get<IRObject>(result.at(1)).members.at("foo").immutable);
+  EXPECT_AS_STRING(std::get<IRObject>(result.at(1)).members.at("foo").pointer,
+                   "/properties/foo");
+  EXPECT_AS_STRING(
+      std::get<IRObject>(result.at(1)).members.at("foo").instance_location,
+      "/foo");
+  EXPECT_FALSE(std::get<IRObject>(result.at(1)).additional.has_value());
+}
+
+TEST(IR_2020_12, object_with_impossible_additional_properties) {
+  const sourcemeta::core::JSON schema{sourcemeta::core::parse_json(R"JSON({
+    "$schema": "https://json-schema.org/draft/2020-12/schema",
+    "type": "object",
+    "properties": {
+      "foo": { "type": "string" }
+    },
+    "additionalProperties": false
+  })JSON")};
+
+  const auto result{
+      sourcemeta::codegen::compile(schema, sourcemeta::core::schema_walker,
+                                   sourcemeta::core::schema_resolver)};
+
+  using namespace sourcemeta::codegen;
+
+  EXPECT_EQ(result.size(), 3);
+
+  EXPECT_TRUE(std::holds_alternative<IRScalar>(result.at(0)));
+  EXPECT_AS_STRING(std::get<IRScalar>(result.at(0)).pointer, "/properties/foo");
+  EXPECT_AS_STRING(std::get<IRScalar>(result.at(0)).instance_location, "/foo");
+  EXPECT_EQ(std::get<IRScalar>(result.at(0)).value, IRScalarType::String);
+
+  EXPECT_TRUE(std::holds_alternative<IRImpossible>(result.at(1)));
+  EXPECT_AS_STRING(std::get<IRImpossible>(result.at(1)).pointer,
+                   "/additionalProperties");
+  EXPECT_AS_STRING(std::get<IRImpossible>(result.at(1)).instance_location,
+                   "/~?additionalProperties~/~P~");
+
+  EXPECT_TRUE(std::holds_alternative<IRObject>(result.at(2)));
+  EXPECT_AS_STRING(std::get<IRObject>(result.at(2)).pointer, "");
+  EXPECT_AS_STRING(std::get<IRObject>(result.at(2)).instance_location, "");
+  EXPECT_EQ(std::get<IRObject>(result.at(2)).members.size(), 1);
+  EXPECT_TRUE(std::get<IRObject>(result.at(2)).members.contains("foo"));
+  EXPECT_FALSE(std::get<IRObject>(result.at(2)).members.at("foo").required);
+  EXPECT_FALSE(std::get<IRObject>(result.at(2)).members.at("foo").immutable);
+  EXPECT_AS_STRING(std::get<IRObject>(result.at(2)).members.at("foo").pointer,
+                   "/properties/foo");
+  EXPECT_AS_STRING(
+      std::get<IRObject>(result.at(2)).members.at("foo").instance_location,
+      "/foo");
+
+  EXPECT_TRUE(std::get<IRObject>(result.at(2)).additional.has_value());
+  EXPECT_FALSE(std::get<IRObject>(result.at(2)).additional->required);
+  EXPECT_FALSE(std::get<IRObject>(result.at(2)).additional->immutable);
+  EXPECT_AS_STRING(std::get<IRObject>(result.at(2)).additional->pointer,
+                   "/additionalProperties");
+  EXPECT_AS_STRING(
+      std::get<IRObject>(result.at(2)).additional->instance_location,
+      "/~?additionalProperties~/~P~");
+}
